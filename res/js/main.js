f95c0e6560c107f5aeef3dd3833f4d70b806661c
Marek Kubelka <116259785+Kokoj1@users.noreply.github.com>
2022-12-19 20:26:38 +0100

    new

--- res/js/main.js
+++ res/js/main.js
@@ -17,11 +17,10 @@ const greenhill=document.getElementById("greenhill");
 const shot = document.getElementById("shot");
 const chemicalplant=document.getElementById("chemicalplant");
 const sky = document.getElementById("skysanctuary");
-const title = document.getElementById("titlesong");
 
 window.onload=function(){
-  title.play();
-}
+  document.getElementById("titlesong").play();
+};
 
 
 let numberOfCookies = 20;
